ee57c232c2e77b640ae225f45a0169507a33075b
Antenor Neto <antenor.neto@kellogg.northwestern.edu>
2024-02-20 05:55:53 +0000

    v2

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,26 @@
 class PlacesController < ApplicationController
+
   def index
-    render :template => "places/index"
+    @places = Place.all
   end
+
+  def show
+    @place = Place.find_by({"id" => params["id"]})
+  end
+
+  def new
+  end
+
+  def create
+    @place = Place.new
+    # use information entered by user
+    @place["name"] = params["name"]
+    @place.save
+    redirect_to "/places"
+  end
+
+  def error
+    render 'not_found', status: :not_found
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
 
   resources "places"
   resources "entries"
+  get("/", { :controller => "articles", :action => "index" })
 
 end
